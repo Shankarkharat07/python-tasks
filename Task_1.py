707ad19dc9e70ca380d4e2b8dc6bbd2f241dc6a7
Shankarkharat07 <shankarkharat07@gmail.com>
2025-04-12 21:04:44 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,7 @@
+a=float(input('Enter the first number: '))
+b=float(input('Enter the second number: '))
+Addition=a+b
+Subtraction=a-b
+Multiplication=a*b
+Division=a/b
+print('Addition: ', Addition ,'\nSubtraction: ',Subtraction,'\nMultiplication :',Multiplication,'\nDivision: ',Division)
---
+++ Task_2.py
@@ -0,0 +1,4 @@
+a=str(input('Enter your First Name: '))
+b=str(input('Enter your Last Name: '))
+fullname=a+" "+b
+print('Hello, ',fullname,'!',"welcome to the python program.")
